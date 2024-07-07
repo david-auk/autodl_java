6ca94b5cf7bff4f4a8bb37781864be1bf44d72b4
david-auk <david@aukes.com>
2024-07-07 20:42:18 +0200

    added iml

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Channel channel = new Channel();
-        channel.setId("UCB0JSO6d5ysH2Mmqz5I9rIw");
-        channel.setTitle("AC/DC");
-        channel.setPullingIncrement(4);
-        channel.setCreatedAt(new Date());
+//        Channel channel = new Channel();
+//        channel.setId("UCB0JSO6d5ysH2Mmqz5I9rIw");
+//        channel.setTitle("AC/DC");
+//        channel.setPullingIncrement(4);
+//        channel.setCreatedAt(new Date());
+//
+//        ChannelDAO.add(channel);
 
-        ChannelDAO.add(channel);
-
-//        for (Channel channel : ChannelDAO.getAll()){
-//            System.out.println(channel.getTitle());
-//        }
+        for (Channel channel : ChannelDAO.getAll()){
+            System.out.println(channel.getTitle());
+        }
     }
 }
